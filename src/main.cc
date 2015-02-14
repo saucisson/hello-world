f424331d96a3055df24784745dca567f440627bf
Alban Linard <alban.linard@lsv.ens-cachan.fr>
2015-02-14 22:25:06 +0100

    Add program that uses TinyCC to show a 'Hello, World'.

---
+++ src/main.cc
@@ -0,0 +1,17 @@
+#include <fstream>
+#include <sstream>
+#include <cstdlib>
+
+int
+main ()
+{
+  std::stringstream program;
+  std::ifstream input ("program.c");
+  program << input.rdbuf();
+  input.close ();
+  std::ofstream output ("hello.c");
+  output << program.str ();
+  output.close ();
+  system ("tcc -run hello.c");
+  return EXIT_SUCCESS;
+}
---
+++ src/program.c
@@ -0,0 +1,11 @@
+/* Hello World program */
+
+#include <stdio.h>
+#include <stdlib.h>
+
+int
+main ()
+{
+  printf ("Hello World\n");
+  return EXIT_SUCCESS;
+}
